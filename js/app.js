941021684ef75911a2461e9d7711ee8b2829166c
pfundaro9 <78462157+pfundaro9@users.noreply.github.com>
2021-04-16 04:51:50 -0400

    Added functional user input

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 function caliRiceRec(){
-    document.getElementById('Cali').style.display = "";
+    var input1 = document.getElementById("userInput").value;
+    var water1 = input1 * 1.6;
+    document.getElementById('output1').innerHTML = "For slightly al dente rice: Combine " + input1 + " cup(s) of rice with " + water1 + " cup(s) of water or broth and 1 Tbsp olive oil. Bring to a boil and stir once to mix. Reduce heat to low, cover with a tight-fitting lid and cook for 25 minutes. Remove from heat and let stand for 5 minutes. Fluff with a fork and serve. For softer rice:Increase liquid by 1/2 cup and cook time by 5 minutes.";
+    document.getElementById('output2').style.display = "";
 }
 
 function whiteRiceRec(){
-    document.getElementById('White').style.display = "";
+    var input2 = document.getElementById("userInput").value;
+    var water2 = input2 * 2;
+    document.getElementById('output2').innerHTML = "Combine " + input2 + " cup(s) of rice with " + water2 + " cup(s) of water and 1 Tbsp olive oil. Bring to a boil, then reduce heat to the lowest setting. Cook for about 18 minutes."
+    document.getElementById("output1").style.display = "";
 }
