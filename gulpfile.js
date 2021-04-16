03160dc2b06bd25151f47a43ded9646a5c89024c
pfundaro9 <78462157+pfundaro9@users.noreply.github.com>
2021-04-16 11:22:08 -0400

    Add jsLinter

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const {src, dest, watch, series} = require(`gulp`);
 const htmlValidator = require(`gulp-html`);
 const htmlCompressor = require(`gulp-htmlmin`);
 const cssLinter = require(`gulp-stylelint`);
+const jsLinter = require(`gulp-eslint`);
 
 
 async function chrome () {
@@ -41,8 +42,33 @@ let lintCSS = () => {
         }));
 };
 
+let lintJS = () => {
+    return src(`dev/js/*.js`)
+        .pipe(jsLinter({
+            parserOptions: {
+                ecmaVersion: 2017,
+                sourceType: `module`
+            },
+            rules: {
+                indent: [2, 4, {SwitchCase: 1}],
+                quotes: [2, `backtick`],
+                semi: [2, `always`],
+                'linebreak-style': [2, `unix`],
+                'max-len': [1, 85, 4]
+            },
+            env: {
+                es6: true,
+                node: true,
+                browser: true
+            },
+            extends: `eslint:recommended`
+        }))
+        .pipe(jsLinter.formatEach(`compact`, process.stderr));
+};
+
 
 
 exports.validateHTML = validateHTML;
 exports.compressHTML = compressHTML;
 exports.lintCSS = lintCSS;
+exports.lintJS = lintJS;
