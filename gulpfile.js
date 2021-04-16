94243c7efc4a5bda0f66a5c966f62a7eb897343f
pfundaro9 <78462157+pfundaro9@users.noreply.github.com>
2021-04-16 11:11:58 -0400

    Add htmlCompressor

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,36 @@
 
+const {src, dest, watch, series} = require(`gulp`);
 const htmlValidator = require(`gulp-html`);
+const htmlCompressor = require(`gulp-htmlmin`);
 
 
+async function chrome () {
+    browserChoice = `google chrome`;
+}
 
-//async function chrome () {
-//    browserChoice = `google chrome`;
-//}
+async function edge () {
+    browserChoice = `microsoft-edge`;
+}
 
-//async function edge () {
-//    browserChoice = `microsoft-edge`;
-//}
-
-//async function allBrowsers () {
-//    browserChoice = [
-//        `google chrome`,
-//        `microsoft-edge`
-//    ];
-//}
+async function allBrowsers () {
+    browserChoice = [
+        `google chrome`,
+        `microsoft-edge`
+    ];
+}
 
 let validateHTML = () => {
     return src(`dev/*.html`)
         .pipe(htmlValidator());
 };
 
+let compressHTML = () => {
+    return src(`dev/*.html`)
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod/`));
+};
+
 
 
 exports.validateHTML = validateHTML;
+exports.compressHTML = compressHTML;
