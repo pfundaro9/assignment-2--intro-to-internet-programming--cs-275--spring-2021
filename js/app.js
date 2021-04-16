52fe7f9dac1ec62a1ab426098ed378cad7df5d53
pfundaro9 <78462157+pfundaro9@users.noreply.github.com>
2021-04-16 01:45:45 -0400

    Added functional buttons

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
-window.onload = () => {
-    alert("hi");
-};
+function caliRiceRec(){
+    document.getElementById('Cali').style.display = "";
+}
+
+function whiteRiceRec(){
+    document.getElementById('White').style.display = "";
+}
